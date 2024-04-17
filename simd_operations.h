d987235395851ec401eab4cca394867a0b60abd2
Roy Cohen <misteroy@gmail.com>
2024-04-17 23:30:31 +0300

    Removed matrix multiply api
    Signed-off-by: Roy Cohen <misteroy@gmail.com>

--- simd_operations.h
+++ simd_operations.h
@@ -7,6 +7,5 @@ void add_floats(float* a, float* b, float* result, ssize_t n);
 void subtract_floats(float* a, float* b, float* result, ssize_t n);
 void multiply_floats(float* a, float* b, float* result, ssize_t n); 
 void divide_floats(float* a, float* b, float* result, ssize_t n);
-void matrix_multiply(float* A, float* B, float* C, ssize_t N);
 
 #endif // SIMD_OPERATIONS_H
--- test_simd_operations.c
+++ test_simd_operations.c
@@ -58,30 +58,3 @@ Test(simd_operations, divide_floats_test) {
     }
 }
 
-/*
-Test(simd_operations, matrix_multiply_test) {
-    const size_t N = 4;
-    float A[N][N] = {{1.0, 2.0, 3.0, 4.0},
-                     {5.0, 6.0, 7.0, 8.0},
-                     {9.0, 10.0, 11.0, 12.0},
-                     {13.0, 14.0, 15.0, 16.0}};
-    float B[N][N] = {{1.0, 0.0, 0.0, 0.0},
-                     {0.0, 1.0, 0.0, 0.0},
-                     {0.0, 0.0, 1.0, 0.0},
-                     {0.0, 0.0, 0.0, 1.0}};
-    float result[N][N] = {0};
-    float expected[N][N] = {{1.0, 2.0, 3.0, 4.0},
-                            {5.0, 6.0, 7.0, 8.0},
-                            {9.0, 10.0, 11.0, 12.0},
-                            {13.0, 14.0, 15.0, 16.0}};
-
-    matrix_multiply((float *)A, (float *)B, (float *)result, N);
-
-    for (size_t i = 0; i < N; i++) {
-        for (size_t j = 0; j < N; j++) {
-            cr_expect_float_eq(result[i][j], expected[i][j], 0.001,
-                               "Element at row %zu, column %zu is %f, expected %f", i, j, result[i][j], expected[i][j]);
-        }
-    }
-}
-*/
